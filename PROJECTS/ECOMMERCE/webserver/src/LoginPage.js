20f099e186de38c3aafec25dc33b572056e31aa9
Anjalypravi <anjalykrishnan2011@gmail.com>
2022-05-10 02:39:06 -0700

    loginpage_html
    login_updated

--- PROJECTS/ECOMMERCE/webserver/src/LoginPage.js
+++ PROJECTS/ECOMMERCE/webserver/src/LoginPage.js
@@ -1,6 +1,35 @@
 function LoginPage(){
     return(
-      <div>Test</div>
+      <div>
+     <head>
+
+<link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+<div class="container">
+    <div class="innerdiv">
+        <div><label><h2>Login</h2></label></div>
+                        
+        <div class= "user"><label><b>Username</b></label>
+            <input type="text" placeholder="Username"/>
+        <div><p></p></div>
+         <label><b>Password</b></label>
+            <input type="text" placeholder="Password" />
+        </div>
+
+        <div class="styleing"><button>Login</button></div>
+
+
+    </div>
+</div>
+
+
+
+
+</body>
+      </div>
     );
   }
   export default LoginPage;
