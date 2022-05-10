31fae9c3f9e7c97126b1a6a9dd1e6a13c2a1fc9c
Anjalypravi <anjalykrishnan2011@gmail.com>
2022-05-10 02:16:56 -0700

    Update Navigation.js

--- PROJECTS/ECOMMERCE/webserver/src/Navigation.js
+++ PROJECTS/ECOMMERCE/webserver/src/Navigation.js
@@ -1,4 +1,10 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
 import LoginPage from "./LoginPage";
+import SignUp from "./SignUp";
+import Dashboard from "./Dashboard";
+import Productlist from "./Productlist";
+
 
 function Navigation(){
   return(
