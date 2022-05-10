20bdf9c2e6140adfe4d9f6febbef9f2c1528f929
Anjalypravi <anjalykrishnan2011@gmail.com>
2022-05-10 04:08:17 -0700

    login htmlcompleted
    direction to sign_up finished

--- PROJECTS/ECOMMERCE/webserver/src/LoginPage.js
+++ PROJECTS/ECOMMERCE/webserver/src/LoginPage.js
@@ -1,49 +1,63 @@
-import "./style.css"
-import {useNavigate} from "react-router-dom"
-function LoginPage(){
-    const Navigate =useNavigate();
-    function newclick(e){
-        e.preventDefault();
-        Navigate("/signup");
-    }
-    return(
-      <div>
-     <head>
+import "./style.css";
+import { useNavigate } from "react-router-dom";
+function LoginPage() {
+  const navigate = useNavigate();
 
-<link rel="stylesheet" href="style.css" />
-</head>
-
-<body>
-
-<div class="container">
-    <div class="innerdiv">
-        <div><label><h2>Login</h2></label></div>
-                        
-        <div class= "user"><label><b>Username</b></label>
-            <input type="text" placeholder="Username"/>
-        <div><p></p></div>
-         <label><b>Password</b></label>
-            <input type="text" placeholder="Password" />
-        </div>
-
-        <div><p></p></div>
-        
-        <div class="styleing"><button>Login</button><p>onclick ={(e)=>{newclick(e);}}classname="new">New user?<p></p></p></div>
-       
-
-
-        <div><p class ="link">New user?</p></div>
-
-        <div><p></p></div>
-
-    </div>
-</div>
-
-
-
-
-</body>
-      </div>
-    );
+  function newclick(e) {
+    e.preventDefault();
+    navigate("/SignUp");
   }
-  export default LoginPage;
+  
+  return (
+    <div>
+      <head>
+        <link rel="stylesheet" href="style.css" />
+      </head>
+
+      <body>
+        <div class="container">
+          <div class="innerdiv">
+            <div>
+              <label>
+                <h2>Login</h2>
+              </label>
+            </div>
+            <div class="user">
+              <label>
+                <b>Username</b>
+              </label>
+              <input type="text" placeholder="Username" />
+              <div>
+                <p></p>
+              </div>
+              <label>
+                <b>Password</b>
+              </label>
+              <input type="text" placeholder="Password" />
+            </div>
+            <div>
+              <p></p>
+            </div>
+            {
+              <div><button>Login</button>
+                <p
+                  onClick={(e)=> {
+                    newclick(e);
+                  } }
+                 className="link"
+                 >
+                  New user?
+                </p>
+              </div>
+            }
+        
+            <div>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+}
+export default LoginPage;
--- PROJECTS/ECOMMERCE/webserver/src/Navigation.js
+++ PROJECTS/ECOMMERCE/webserver/src/Navigation.js
@@ -1,24 +1,21 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LoginPage from "./LoginPage";
 import SignUp from "./SignUp";
 import Dashboard from "./Dashboard";
 import Productlist from "./Productlist";
 
-
-function Navigation(){
-  return(
+function Navigation() {
+  return (
     <div>
-      
       <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />}></Route>
-      <Route path="/SignUp" element={<SignUp />}></Route>
-      <Route path="/Dashboard" element={<Dashboard />}></Route>
-      <Route path="/Productlist" element={<Productlist />}></Route>
-      
-    </Routes>
-  </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/SignUp" element={<SignUp />}></Route>
+          <Route path="/Dashboard" element={<Dashboard />}></Route>
+          <Route path="/Productlist" element={<Productlist />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
