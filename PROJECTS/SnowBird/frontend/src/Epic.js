aedaf6bc351531cf39bc2160622e261bbaecdabe
Anjalypravi <anjalykrishnan2011@gmail.com>
2022-05-16 01:14:29 -0700

    epics

---
+++ PROJECTS/SnowBird/frontend/src/Epic.js
@@ -0,0 +1,50 @@
+import "./styles/Epicstyle.css";
+function Epic() {
+  return (
+    <div>
+      <div className="outer">
+        {/* USer name with icon */}
+        <div className="firstrow">
+          <div className="usericon"></div>
+          <label>User</label>
+        </div>
+        <div className="secondrow">
+          {/* Side navigation menu */}
+          <div className="firstcolumn">
+            <nav>
+              <li>Board</li>
+              <li>Projects</li>
+              <li>Epics</li>
+              <li>Tasks</li>
+              <li>Sprints</li>
+              <li>Users</li>
+            </nav>
+          </div>
+
+          {/* Main outline */}
+          <div className="secondcolumn">
+            <div className="seccolumfirstrow">
+              <label>
+                <h2>Epic</h2>
+              </label>
+              <button className="buttonright">Create New</button>
+            </div>
+            <div className="seccolumsecondrow">
+              <table>
+                <thead>
+                  <th>#id</th> <th>EpicName</th> <th>Status</th>{" "}
+                  <th>ProjectName</th>
+                </thead>
+                <tbody>
+                    <tr><td>10</td>   <td>User1</td>  <td>ToDo</td>  <td>Ecommerce</td></tr>
+                    <tr><td>11</td>   <td>User2</td>  <td>ToDo</td>  <td>Ecommerce</td></tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Epic;
--- PROJECTS/SnowBird/frontend/src/index.js
+++ PROJECTS/SnowBird/frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Epic from './Epic';
 import LoginPage from './LoginPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginPage/>
+    <Epic/>
   </React.StrictMode>
 );
 
