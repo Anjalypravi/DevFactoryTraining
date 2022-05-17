3cf067baa98fac80eb504028787d0f62b3f922b8
Anjalypravi <anjalykrishnan2011@gmail.com>
2022-05-17 01:53:20 -0700

    complete edit epic

---
+++ PROJECTS/SnowBird/frontend/src/EditEpic.js
@@ -0,0 +1,79 @@
+import "./styles/Epicstyle.css";
+function EditEpic() {
+  return (
+    <div>
+      <div className="outer">
+        {/* USer name with icon */}
+        <div className="firstrow">
+          <div className="usericon"></div>
+          <label>User</label>
+        </div>
+        <div className="secondrow">
+          {/* Side navigation menu */}
+          <div className="firstcolumn">
+            <nav>
+              <li>Board</li>
+              <li>Projects</li>
+              <li>Epics</li>
+              <li>Tasks</li>
+              <li>Sprints</li>
+              <li>Users</li>
+            </nav>
+          </div>
+
+          {/* Main outline */}
+            <div className="secondcolumn">
+            <div className="buttonright">
+              <label>EditEpic</label>
+              <button>SAVE</button>
+            </div>
+            <div className="seccolumsecondrow">
+              <div>
+                <lable>
+                  <h4>Title</h4>
+                </lable>
+                <input className="titleinput" type="text"></input>
+              </div>
+              <div>
+                <lable>
+                  <h4>Description</h4>
+                </lable>
+                <input className="descriptioninput" type="text"></input>
+              </div>
+              <div className="statusinput">
+                <lable>
+                  <h4>Status</h4>
+                </lable>
+                <select className="select1" id="status-select">
+    <option value="">-- option--</option>
+    <option value="ToDo">ToDo</option>
+    <option value="InProgress">InProgress</option>
+    <option value="Review">Review</option>
+    <option value="Complete">Complete</option>
+    
+</select>
+
+              </div>
+              <div className="assignedtoinput">
+                <lable>
+                  <h4>Assigned to</h4>
+                </lable>
+                <select className="select2" id="Name-select">
+    <option value="">-- option--</option>
+    <option value="Ajay">Ajay</option>
+    <option value="Midhun">Midhun</option>
+    <option value="Chithra">Chithra</option>
+    
+</select>
+</div>
+                
+                
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default EditEpic;
