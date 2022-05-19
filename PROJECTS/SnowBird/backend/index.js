d17efe616f4b837482afe5b33cee495f2d2db507
Anjalypravi <anjalykrishnan2011@gmail.com>
2022-05-18 23:42:56 -0700

    index.js
    userfetch&validation

---
+++ PROJECTS/SnowBird/backend/index.js
@@ -0,0 +1,60 @@
+var mysql = require("mysql");
+const express = require('express');
+const { Console } = require('console');
+const app = express();
+const port = 8000;
+app.use(express.json());
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "prjdb"
+});
+
+con.connect(function (err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("connected");
+    }
+});
+app.post('/userfetch', function (req, res) {
+
+    var y = "SELECT txtUserName,txtPassword,refUserRole from tblUsers ";
+    con.query(y, function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.post('/uservalidation', function (req, res) {
+    
+    var a = req.body.txtUserName;
+    var b = req.body.txtPassword;
+
+    
+    var sql = "SELECT IFNULL((SELECT  refUserRole from tblUsers where txtUserName ='" + a + "' AND txtPassword ='" + b + "'),'Not an Existing User') AS VAL" ;
+    con.query(sql, function (err, result, fields) {
+
+        if (err) {
+
+            throw err;
+        }else
+        {
+            console.log(result);
+            res.send(result);
+
+        }
+    });
+});
+
+ 
+app.listen(port, () => {
+    console.log(`Server is running`)
+}
+//);
+//});
+)
